53cbaee0c8f5841d29b82d40ed88adf54731b750
LianHuaa <109400802+LianHuaa@users.noreply.github.com>
2024-04-10 22:00:49 +0800

    Add files via upload

---
+++ js/background.js
@@ -0,0 +1,19 @@
+let catalogue = "./img/";
+
+let suffix = ".png";
+
+let num = 6;
+
+let num_random = Math.floor(Math.random() * num);
+
+(function () {
+  document.body.style.backgroundImage = `url(${catalogue}${num_random}${suffix})`
+  return document.body.style.backgroundImage;
+}());
+
+const Button = document.querySelector('#butt');
+const topnav = document.querySelector('.topnav');
+
+Button.addEventListener('click', function () {
+  topnav.classList.toggle('topnav_modify');
+});
