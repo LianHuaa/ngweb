ff1aa0a13bd2bc4b96b36c7436d2396e1783f967
LianHuaa <109400802+LianHuaa@users.noreply.github.com>
2024-04-11 15:23:26 +0800

    更新 background.js

--- js/background.js
+++ js/background.js
@@ -1,18 +1,11 @@
-const catalogue = "./img/";
-
-const suffix = ".png";
-
-const num = 3;
-
-const num_random = Math.floor(Math.random() * num);
-
 (function () {
-  document.body.style.backgroundImage = `url(${catalogue}${num_random}${suffix})`
+    const catalogue = "./img/";
+    const suffix = ".png";
+    const num = 3;
+    const num_random = Math.floor(Math.random() * num);
+    document.body.style.backgroundImage = `url(${catalogue}${num_random}${suffix})`
 }());
 
-const Button = document.querySelector('#butt');
-const topnav = document.querySelector('.topnav');
-
-Button.addEventListener('click', function () {
-  topnav.classList.toggle('topnav_modify');
+document.querySelector('#butt').addEventListener('click', function () {
+    document.querySelector('.topnav').classList.toggle('topnav_modify');
 });
